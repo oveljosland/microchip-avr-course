2fd42d76ae299f1f66d1a0b5eaad70a4c326955d
Ove Ljosland <oveljosland@icloud.com>
2025-03-30 16:03:45 +0200

    added task 'menu system'

---
+++ day3/menusystem/main.c
@@ -0,0 +1,30 @@
+/** 
+* @file main.c
+* @author oveljosland
+* @date 2025-03-05
+* @brief menu system 
+*/
+
+#include "config.h"
+#include "display/ssd1306.h" /* display driver */
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
+#include <avr/cpufunc.h>
+
+/* TODO: make an interactive menu system using buttons and other peripherals */
+
+/* tip: maybe control the LED ring through a menu? */
+/* tip: choose which font you want to use in config.h */
+
+/* hint: take a look at the included header file for the display driver */
+/* hint: also take a look at https://github.com/Matiasus/SSD1306 */
+
+/* you are on your own here, good luck! */
+
+
+int main(void)
+{
+    return 0;
+};
