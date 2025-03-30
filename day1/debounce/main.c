2dbbacd28228b3fbbf5226dd520dfc7b694465e2
Ove Ljosland <oveljosland@icloud.com>
2025-03-30 14:55:10 +0200

    added extra task for day 1

---
+++ day1/debounce/main.c
@@ -0,0 +1,25 @@
+/** 
+* @file main.c
+* @author oveljosland
+* @date 2025-03-03
+* @brief use buttons on the Explorer board to control LEDs
+*/
+
+/* TODO: define CPU clock frequency */
+/* TODO: include the necessary header files */
+/* TODO: define pins or use bitmasks */
+
+/* no answer key for this one :) */
+
+/* main: use buttons on the Explorer-board to control LEDs */
+int main(void)
+{
+    /* TODO: configure buttons and LEDs */
+    /* hint: do you need to configure anything else? */
+
+    while (1) {
+        /* TODO: check if buttons are pressed */
+        /* TODO: toggle LEDs */
+    }
+    return 0;
+};
