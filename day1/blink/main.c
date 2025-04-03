2ac037ed9056c0d6fd158afabe2431334e0d2194
Ove Ljosland <oveljosland@icloud.com>
2025-04-03 11:32:42 +0200

    cleanup task 'blink'

--- day1/blink/main.c
+++ day1/blink/main.c
@@ -2,20 +2,20 @@
 * @file main.c
 * @author oveljosland
 * @date 2025-03-03
-* @brief blink onboard LED0
+* @brief blink onboard LEDs
 */
 
 #define F_CPU 4000000UL
 #include <avr/io.h>
 #include "util/delay.h"
 
-/* main: blink onboard LED0 */
+/* main: blink onboard LEDs */
 int main(void)
 {
-    /* set LED0 as output */
+    /* set LED0 or any other LED as output */
 
     while (1) {
-        /* blink led */
+        /* blink LED */
     }
 
     return 0;
