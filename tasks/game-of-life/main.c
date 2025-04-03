fea5f9cbb6097c9c9be4455ad14c95db747b4f3f
Ove Ljosland <oveljosland@icloud.com>
2025-04-03 14:38:43 +0200

    added game-of-life

---
+++ tasks/game-of-life/main.c
@@ -0,0 +1,14 @@
+/** 
+ * @file main.c
+ * @author ove
+ * @date 2025-04-03
+ * @brief conway's game of life 
+ */
+
+int main(void)
+{
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+};
