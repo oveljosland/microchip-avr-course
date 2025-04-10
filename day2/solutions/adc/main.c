e9cb7e29d4407e07a06f11bc20354aa797334ed4
Henrik Moe Arnesen <HenrikMoe.Arnesen@microchip.com>
2025-04-10 15:00:28 +0200

    Add scrolling ADC value display to better show current read value, add scaling of read value and update comments for clarity

--- day2/solutions/adc/main.c
+++ day2/solutions/adc/main.c
@@ -20,15 +20,27 @@ int main(void)
 
     SSD1306_Init(SSD1306_ADDR);
     SSD1306_ClearScreen();
+
+    // Finish the adc_init() function in the 'peripherals' folder and call it here
     adc_init();
 
-    uint8_t value = 0;
+    uint16_t value = 0;
+    uint8_t values[MAX_X]; // Buffer to hold the 128 latest ADC reads
 
     while (1) {
+        // Read from adc
+        value = adc_read();
+        // Try scaling the adc readings to fit the OLED display
+        value = value >> 4; // Bit shift right 4 times to divide by 16
+        // Add value to last index of buffer
+        values[MAX_X-1] = (uint8_t)value;
+
         SSD1306_ClearScreen();
-        for (uint8_t i = 0; i < MAX_X; i++) {     
-            value = adc_read();
-            SSD1306_DrawLine(i, i, MAX_Y, MAX_Y - value); /* display is flipped */
+
+        // Draw the buffered values as lines on the display
+        for (uint8_t i = 1; i < MAX_X; i++) {
+            SSD1306_DrawLine(i-1, i-1, MAX_Y, MAX_Y - values[i-1]); // Display start with 0,0 at top left
+            values[i-1] = values[i]; // Shift the values buffer to the left
         }
         SSD1306_UpdateScreen(SSD1306_ADDR);
     }    
